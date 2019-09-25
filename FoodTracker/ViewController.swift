192e513c3d60f89bed5909f4bd17ac5c5431e227
rikaguri <kirarika7@gmail.com>
2019-09-25 22:26:42 +0900

    up

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    //MARK: PropertiesFo
+    //MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
